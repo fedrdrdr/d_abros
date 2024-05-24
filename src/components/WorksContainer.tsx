91aafd8e5b3218f92e377ee0a06112e5762fa7b6
Makapenko <makap88@gmail.com>
2024-05-24 16:20:26 +0700

    feat: WorkContainer is works now

--- src/components/WorksContainer.tsx
+++ src/components/WorksContainer.tsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from 'react';
 import WorkCard from './WorkCard';
 import { styled } from '@mui/system';
 import { Container, Grid } from '@mui/material';
-import worksExample from '../assets/worksExample'
+import { getPosts } from '../components/auth/firebaseUtils';
+import { Post } from '../assets/types';
 
 function WorksContainer() {
 
@@ -10,15 +12,25 @@ function WorksContainer() {
     justifyContent: 'center'
   });
 
+  const [posts, setPosts] = useState<Post[]>([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const fetchedPosts = await getPosts();
+      setPosts(fetchedPosts);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <WorksContainer>
       <Grid container spacing={2}>
-        {worksExample.map((work, index) => (
+        {posts.map((post, index) => (
           <Grid item xs={12} sm={6} md={4} key={index}>
             <WorkCard
-              image={work.image}
-              title={work.title}
-              description={work.description}
+              image={post.photoURL}
+              title={post.title}
+              description={post.description}
             />
           </Grid>
         ))}
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,15 +3,11 @@ import Footer from '../components/Footer';
 import WorksContainer from '../components/WorksContainer';
 import Header from '../components/Header';
 import CreatePost from './CreatePost';
-import PostsList from './PostsList';
-
-
 
 const MainPage: React.FC = () => {
   return (
     <>
       <CreatePost />
-      <PostsList />
       <Header />
       <WorksContainer />
       <Footer />
